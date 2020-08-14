8e557543025a2b873d86d75281b5dcf3282ff70b
RLant <rebeccalant@gmail.com>
2020-08-13 23:13:25 -0400

    Adding Plugins, lesson 2 pg 13

--- evaluate-news-nlp/webpack.config.js
+++ evaluate-news-nlp/webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path")
 const webpack = require("webpack")
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   entry: './src/client/index.js',
   module: {
@@ -8,11 +10,14 @@ module.exports = {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
-            },
-            {
-                test: /\.scss$/,
-                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
             }
         ]
-          }
-                }
+
+  },
+  plugins: [
+      new HtmlWebPackPlugin({
+          template: "./src/client/views/index.html",
+          filename: "./index.html",
+      })
+  ]
+}
